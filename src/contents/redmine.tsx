c1ec850e8138f5f1b8df06177d80020aada96dcc
Peng <zbp9909@gmail.com>
2025-09-18 18:15:01 +0800

    feat：增加左侧菜单栏

--- src/contents/redmine.tsx
+++ src/contents/redmine.tsx
@@ -1,9 +1,6 @@
 // import redmineCss from "data-text:~/assets/redmine.css"
 import $ from "jquery"
 import type { PlasmoCSConfig } from "plasmo"
-import { useEffect, useState } from "react"
-
-import "src/styles/tailwind.css" // 引入tailwind
 
 export const config: PlasmoCSConfig = {
   matches: ["*://*.yzrdm.cdleadus.com/*", "*://*.192.168.1.168/*"],
