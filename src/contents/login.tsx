e50e5d484b4eb6a2662b742e7dbe5afec9dd9646
Peng <zbp9909@gmail.com>
2025-09-30 11:42:30 +0800

    feat：增加登录页背景图片，完善登录表单

--- src/contents/login.tsx
+++ src/contents/login.tsx
@@ -8,15 +8,41 @@ export const config: PlasmoCSConfig = {
   all_frames: true
 }
 
+function appendRegisterLink() {
+  const loginForm = $("#login-form form")
+  if (loginForm.length) {
+    const registerLink = $(
+      `
+      <div>
+        <a class="register" href="/account/register">注册</a>
+        <a class="lost_password" href="/account/lost_password">忘记密码</a>
+      </div>
+      `
+    )
+    loginForm.append(registerLink)
+  }
+}
+
 const Login = () => {
   useEffect(() => {
-    const loginForm = $("#login-form form")
-    if (loginForm.length) {
-      const registerLink = $(
-        '<a class="register" href="/account/register">注册</a>'
-      )
-      loginForm.append(registerLink)
-    }
+    $(".lost_password").remove()
+
+    const pop_top_img_url = chrome.runtime.getURL("assets/images/login-bg.png")
+    $("#wrapper").css({
+      "background-image": `url(${pop_top_img_url})`,
+      "background-repeat": "no-repeat",
+      "background-position": "center center",
+      "background-size": "cover"
+    })
+    $("#username").attr("placeholder", "请输入用户名")
+    $("#password").attr("placeholder", "请输入密码")
+
+    // 在 #login-form 头部插入欢迎标题
+    const welcomeTitle = $("<h1 style='margin-bottom: 20px;'>欢迎使用</h1>")
+    $("#login-form").prepend(welcomeTitle)
+
+    appendRegisterLink()
+    $("#login-form").show()
   }, [])
 }
 
